5dfd90e158c3e2307422bd792ee6eb15f49cef9e
NikitaSirodoev <44817556+NikitaSirodoev@users.noreply.github.com>
2020-11-28 16:02:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
+dotenv.config({path: './.env'})
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+var users = [];
+
+io.on("connection", function (socket){
+    console.log('user_connected', socket.id);
+
+    socket.on("user_connected", function(name){
+        users[name] = socket.id;
+        io.emit('user_connected', name);
+    })
+})
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+const publicDirectory = path.join(__dirname, './public');
+
+app.use(express.static(publicDirectory));
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.set('view engine' , 'hbs');
+
+db.connect((error)=> {
+    if(error){
+        console.log(error)
+    }else{
+        console.log("MySql Connected...")
+    }
+})
+
+//Define Routes
+app.use('/', require('./routes/pages'))
+
+app.use('/auth', require('./routes/auth'))
+
+http.listen(5000, () => {
+    console.log("Server started on Port 5000")
+})
+
+
+
